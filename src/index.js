d3cb6933ea7e71c6ce14e0d47a745bbc9f362128
George Burt <georgeburt@Georges-MacBook-Air.local>
2022-05-17 18:12:42 -0400

    Hello short circuit  implementing

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const Button = (props) => {
 };
 
 const Application = (props) => {
-  const [name, setName] = useState("")
+  const [name, setName] = useState("");
 
   const reset = (props) => {
     setName(name => name = "");
@@ -25,9 +25,8 @@ const Application = (props) => {
         placeholder="Type your name"
       />
       <main>
-        {/* your code here -- this entire line including the curly braces can be removed */}
         <Button name={name} setName={setName} reset={reset}/>
-        <h1>Hello {name}</h1>
+        {name ? <h1>Hello {name}</h1> : null}
       </main>
     </>
   );
